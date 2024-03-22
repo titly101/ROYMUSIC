9e4c80c916bcbcd2eab1a30b08caa54b4701910f
『ʟᴏᴄᴏ』﹤/3 『ᴘɪʟᴏᴛ』 <155711756+LOCO-PILOT@users.noreply.github.com>
2024-03-22 21:50:21 +0530

    Update info.py

--- DAXXMUSIC/plugins/misc/info.py
+++ DAXXMUSIC/plugins/misc/info.py
@@ -2,16 +2,18 @@ import asyncio, os, time, aiohttp
 from pathlib import Path
 from PIL import Image, ImageDraw, ImageFont
 from asyncio import sleep
-from DAXXMUSIC import app as Hiroko
+from DAXXMUSIC import app
 from pyrogram import filters, Client, enums
 from pyrogram.enums import ParseMode
 from pyrogram.types import *
+from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardMarkup, InlineKeyboardButton
 from typing import Union, Optional
 
-
-
-# --------------------------------------------------------------------------------- #
-
+EVAA = [
+    [
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/nykaaxbot?startgroup=true"),
+    ],
+]
 
 get_font = lambda font_size, font_path: ImageFont.truetype(font_path, font_size)
 resize_text = (
@@ -39,18 +41,12 @@ async def get_userinfo_img(
 
         circular_img = Image.new("RGBA", img.size, (0, 0, 0, 0))
         circular_img.paste(img, (0, 0), mask)
-        resized = circular_img.resize((400, 400))
-        bg.paste(resized, (440, 160), resized)
+        resized = circular_img.resize((534, 534))
+        bg.paste(resized, (607, 86), resized)
 
     img_draw = ImageDraw.Draw(bg)
 
-    img_draw.text(
-        (529, 627),
-        text=str(user_id).upper(),
-        font=get_font(46, font_path),
-        fill=(255, 255, 255),
-    )
-
+    
 
     path = f"./userinfo_img_{user_id}.png"
     bg.save(path)
@@ -59,31 +55,32 @@ async def get_userinfo_img(
 
 # --------------------------------------------------------------------------------- #
 
-bg_path = "DAXXMUSIC/assets/userinfo.png"
+bg_path = "DAXXMUSIC/assets/INFORMATION2.PNG"
 font_path = "DAXXMUSIC/assets/hiroko.ttf"
 
+#
 # --------------------------------------------------------------------------------- #
 
 
 INFO_TEXT = """
 ㅤ✦ ᴜsᴇʀ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ✦
-•❅─────✧❅✦❅✧─────❅•
-      
-๏ ᴜsᴇʀ ɪᴅ ➠ {}
-๏ ᴜsᴇʀ ɴᴀᴍᴇ ➠ {}
+❅─────✧❅✦❅✧─────❅
+  
+๏ ᴜsᴇʀ ɪᴅ ➠ `{}`
 ๏ ᴜsᴇʀɴᴀᴍᴇ ➠ @{}
-๏ ғʀɪsᴛ ɴᴀᴍᴇ ➠ {}
-๏ ᴜsᴇʀ sᴛᴀᴛᴜs ➠ {}
+๏ ᴍᴇɴᴛɪᴏɴ ➠ {}
+๏ ᴜsᴇʀ sᴛᴀᴛᴜs ➠ `{}`
 ๏ ᴜsᴇʀ ᴅᴄ ɪᴅ ➠ {}
 ๏ ᴜsᴇʀ ʙɪᴏ ➠ {}
 
-๏ ᴍᴀᴅᴇ ʙʏ ➠ [ʀᴏʏ-ᴇᴅɪᴛx](https://t.me/roy_editx)"""
+๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ [๛ɴ ʏ ᴋ ᴀ ᴀ࿐](https://t.me/nykaaxbot)
+"""
 
 # --------------------------------------------------------------------------------- #
 
 async def userstatus(user_id):
    try:
-      user = await Hiroko.get_users(user_id)
+      user = await app.get_users(user_id)
       x = user.status
       if x == enums.UserStatus.RECENTLY:
          return "User was seen recently."
@@ -96,12 +93,12 @@ async def userstatus(user_id):
       elif x == enums.UserStatus.ONLINE:
          return "User is online."
    except:
-        return "**sᴏᴍᴇᴛʜɪɴɢ ᴡʀᴏɴɢ ʜᴀᴘᴘᴇɴᴇᴅ !**"
+        return "**✦ sᴏᴍᴇᴛʜɪɴɢ ᴡʀᴏɴɢ ʜᴀᴘᴘᴇɴᴇᴅ !**"
     
 
 # --------------------------------------------------------------------------------- #
 
-@Hiroko.on_message(filters.command(["info", "userinfo"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
+@app.on_message(filters.command(["info", "information", "userinfo"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
 async def userinfo(_, message):
     chat_id = message.chat.id
     user_id = message.from_user.id
@@ -109,8 +106,8 @@ async def userinfo(_, message):
     if not message.reply_to_message and len(message.command) == 2:
         try:
             user_id = message.text.split(None, 1)[1]
-            user_info = await Hiroko.get_chat(user_id)
-            user = await Hiroko.get_users(user_id)
+            user_info = await app.get_chat(user_id)
+            user = await app.get_users(user_id)
             status = await userstatus(user.id)
             id = user_info.id
             dc_id = user.dc_id
@@ -118,22 +115,22 @@ async def userinfo(_, message):
             username = user_info.username
             mention = user.mention
             bio = user_info.bio
-            photo = await Hiroko.download_media(user.photo.big_file_id)
+            photo = await app.download_media(user.photo.big_file_id)
             welcome_photo = await get_userinfo_img(
                 bg_path=bg_path,
                 font_path=font_path,
                 user_id=user_id,
                 profile_path=photo,
             )
-            await Hiroko.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
-                id, name, username, mention, status, dc_id, bio), reply_to_message_id=message.id)
+            await app.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
+                id, username, mention, status, dc_id, bio), reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(EVAA),)
         except Exception as e:
             await message.reply_text(str(e))        
       
     elif not message.reply_to_message:
         try:
-            user_info = await Hiroko.get_chat(user_id)
-            user = await Hiroko.get_users(user_id)
+            user_info = await app.get_chat(user_id)
+            user = await app.get_users(user_id)
             status = await userstatus(user.id)
             id = user_info.id
             dc_id = user.dc_id
@@ -141,15 +138,15 @@ async def userinfo(_, message):
             username = user_info.username
             mention = user.mention
             bio = user_info.bio
-            photo = await Hiroko.download_media(user.photo.big_file_id)
+            photo = await app.download_media(user.photo.big_file_id)
             welcome_photo = await get_userinfo_img(
                 bg_path=bg_path,
                 font_path=font_path,
                 user_id=user_id,
                 profile_path=photo,
             )
-            await Hiroko.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
-                id, name, username, mention, status, dc_id, bio), reply_to_message_id=message.id)
+            await app.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
+                id, username, mention, status, dc_id, bio), reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(EVAA),)
         except Exception as e:
             await message.reply_text(str(e))
 
@@ -157,8 +154,8 @@ async def userinfo(_, message):
     elif message.reply_to_message:
         user_id = message.reply_to_message.from_user.id
         try:
-            user_info = await Hiroko.get_chat(user_id)
-            user = await Hiroko.get_users(user_id)
+            user_info = await app.get_chat(user_id)
+            user = await app.get_users(user_id)
             status = await userstatus(user.id)
             id = user_info.id
             dc_id = user.dc_id
@@ -166,16 +163,19 @@ async def userinfo(_, message):
             username = user_info.username
             mention = user.mention
             bio = user_info.bio
-            photo = await Hiroko.download_media(message.reply_to_message.from_user.photo.big_file_id)
+            photo = await app.download_media(message.reply_to_message.from_user.photo.big_file_id)
             welcome_photo = await get_userinfo_img(
                 bg_path=bg_path,
                 font_path=font_path,
                 user_id=user_id,
                 profile_path=photo,
             )
-            await Hiroko.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
-                id, name, username, mention, status, dc_id, bio), reply_to_message_id=message.id)
+            await app.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
+                id, username, mention, status, dc_id, bio), reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(EVAA),)
         except Exception as e:
             await message.reply_text(str(e))
 
 ####
+
+
+
