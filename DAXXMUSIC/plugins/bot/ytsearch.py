2343b5e10b07fbb5c94f4b608f485ed54230ca85
『ʟᴏᴄᴏ』﹤/3 『ᴘɪʟᴏᴛ』 <155711756+LOCO-PILOT@users.noreply.github.com>
2024-03-22 21:34:03 +0530

    Update ytsearch.py

--- DAXXMUSIC/plugins/bot/ytsearch.py
+++ DAXXMUSIC/plugins/bot/ytsearch.py
@@ -9,25 +9,25 @@ from config import BOT_USERNAME
 
 
 
-@app.on_message(filters.command("search"))
+@app.on_message(filters.command("yt"))
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("/search needs an argument!")
+            await message.reply_text("✦ /yt needs an argument!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text(" searching")
+        m = await message.reply_text("✦ searching....")
         results = YoutubeSearch(query, max_results=5).to_dict()
         i = 0
         text = ""
         while i < 5:
-            text += f"๏ ᴠɪᴅᴇᴏ ɴᴀᴍᴇ ➛ {results[i]['title']}\n"
-            text += f"๏ ᴠɪᴅᴇᴏ ᴅᴜʀᴀᴛɪᴏɴ ➛ {results[i]['duration']}\n"
-            text += f"๏ ᴠɪᴅᴇᴏ ᴠɪᴇᴡs ➛ {results[i]['views']}\n"
-            text += f"๏ ᴠɪᴅᴇᴏ ᴄʜᴀɴɴᴇʟ ➛ {results[i]['channel']}\n"
-            text += f"๏ ᴠɪᴅᴇᴏ ᴜʀʟ ➛ https://www.youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"✦ ᴠɪᴅᴇᴏ ɴᴀᴍᴇ ➠ {results[i]['title']}\n\n"
+            text += f"๏ ᴠɪᴅᴇᴏ ᴅᴜʀᴀᴛɪᴏɴ ➠ {results[i]['duration']}\n"
+            text += f"๏ ᴠɪᴅᴇᴏ ᴠɪᴇᴡs ➠ {results[i]['views']}\n"
+            text += f"๏ ᴠɪᴅᴇᴏ ᴄʜᴀɴɴᴇʟ ➠ {results[i]['channel']}\n"
+            text += f"๏ ᴠɪᴅᴇᴏ ᴜʀʟ ➠ https://www.youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
         await m.edit(str(e))
-        
+
